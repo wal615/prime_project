2e4353d5162ab6e6ebb6557b3a1098de3b6c1c17
xuelong wang <Ben@10-13-223-241.vlan868.client.wireless.uic.edu>
2017-11-22 08:31:10 -0600

    delete papers

--- inverse_regression/R_code/simulation_helpers.R
+++ inverse_regression/R_code/simulation_helpers.R
@@ -28,13 +28,44 @@ slice_data_with_index <- function(x, y, slice_number){
   
   ordered_data[,slice:= slice] %>% 
     data.table::setorder(., index) %>%
-    set(., j = "index", value = NULL) %>%
+    data.table::set(., j = "index", value = NULL) %>%
         data.frame(.) 
 }
 
 #### generate the simulation data based on the given profile 
 #### return a list of data_file names
 
-generate_simulation <- function(n, p, name_list) {
+generate_simulation <- function(n, p, sim_string, slice_number, root_path) {
+  glue(sim_string) %>% parse(text = .) %>% eval()
+  y_names <- names(y)
+  file_list <- paste0(root_path, deparse(substitute(sim_string)), "_", y_names, ".csv")
+  
+  cat("write the simulation data into disk...\n")
+  for(i in length(y)) {
+    y_i <- y[[i]] %>% as.numeric(.) # data.table need the y to be a vector instead of matrix
+    slice_data_with_index(x, y_i, slice_number) %>% write.csv(., file = file_list[i], row.names = FALSE)
+  }
+  
+  cat("Done\n")
+  
+  file_list
+  }
 
-}
+
+Li_2009 <- "
+cat('Generating simulation data...\n')
+set.seed(1014)
+p <- {p} # demension
+n <- {n}
+e <- rnorm(n)
+sigma <- 0.2
+x <- rnorm(p*n) %>% matrix(., ncol = p)
+beta_1 <- matrix(c(1,1,1,0,0,0),ncol = 1)
+beta_2 <- matrix(c(1,0,0,0,1,3),ncol = 1)
+y_1 <- 0.4*(x%*%beta_1)^2 + 3*sin(x%*%beta_2/4) + sigma*e
+y_2 <- 3*sin(x%*%beta_1/4) + 3*sin(x%*%beta_2/4) + sigma*e
+y_3 <- 0.4*(x%*%beta_1)^2 + (x%*%beta_2) %>% abs(.) %>% sqrt(.) + sigma*e
+y_4 <- 3*sin(x%*%beta_2/4) + (1+x%*%beta_2)*sigma*e
+y <- list(y_1 = y_1, y_2 = y_2, y_3 = y_3, y_4 = y_4)
+"
+
