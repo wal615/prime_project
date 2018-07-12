979c96a80e847720afb0b5866ed0d1aab684fa98
xuelong <xuelong@statlab.(none)>
2018-07-12 12:44:57 -0500

    -modified on categorized data

--- Chen_environmental_study/R_code/simulation_categorize.R
+++ Chen_environmental_study/R_code/simulation_categorize.R
@@ -32,16 +32,6 @@ b_cut_5 <- std_fn(b_cut_5, ncol(b_cut_5), tran_FUN = null_tran)
 data_list_cut <- list(b_cut_2 = b_cut_2,
                   b_cut_5 = b_cut_5)
 
-## cut with quantile 2 interval
-b_quantile_2 <- apply(b,2, FUN = cut_fn, by = 0.5)
-b_quantile_2 <- std_fn(b_quantile_2, ncol(b_quantile_2), tran_FUN = null_tran)
-
-b_quantile_5 <- apply(b,2, FUN = cut_fn, by = 0.2)
-b_quantile_5 <- std_fn(b_quantile_2, ncol(b_quantile_5), tran_FUN = null_tran)
-
-data_list_quantile <- list(b_quantile_2 = b_quantile_2,
-                      b_quantile_5 = b_quantile_5)
-
 interaction_list <- list(1,1)
 interaction_m_list <- list(1,1)
 
@@ -55,11 +45,34 @@ result_list <- mapply(FUN = compare_corr_GCTA,
 
 save(result_list, file = "./result/simulation_cut_2_5")
 
+
+
+
+## cut with quantile method
+
+b_quantile_2 <- apply(b,2, FUN = cut_fn, by = 0.5)
+b_quantile_2 <- std_fn(b_quantile_2, ncol(b_quantile_2), tran_FUN = null_tran)
+
+b_quantile_5 <- apply(b,2, FUN = cut_fn, by = 0.2)
+b_quantile_5 <- std_fn(b_quantile_5, ncol(b_quantile_5), tran_FUN = null_tran)
+
+b_quantile_10 <- apply(b,2, FUN = cut_fn, by = 0.1)
+b_quantile_10 <- std_fn(b_quantile_10, ncol(b_quantile_10), tran_FUN = null_tran)
+
+data_list_quantile <- list(b_quantile_2 = b_quantile_2,
+                      b_quantile_5 = b_quantile_5,
+                      b_quantile_10 = b_quantile_10)
+
+interaction_list <- list(1,1,1)
+interaction_m_list <- list(1,1,1)
+
+
+
 result_list <- mapply(FUN = compare_corr_GCTA,
                       b = data_list_quantile,
                       interaction = interaction_list,
                       interaction_m = interaction_m_list,
-                      MoreArgs = list(brep = 80, nrep = 20, seed = 123, cores = 2),
+                      MoreArgs = list(brep = 80, nrep = 20, seed = 123, cores = 3),
                       SIMPLIFY = FALSE)
 
-save(result_list, file = "./result/simulation_quantile_2_5")
+save(result_list, file = "./result/simulation_quantile_2_5_10")
