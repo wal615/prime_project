7558c4f6ba7b1e9ce95785ea2edccbeebde07f2b
xuelong <xuelong@statlab.(none)>
2018-08-01 11:15:28 -0500

    -add simulation result

--- Chen_environmental_study/R_code/Yang_REML.R
+++ Chen_environmental_study/R_code/Yang_REML.R
@@ -83,7 +83,7 @@ Yang=function(y,x,interact=0){
 }
 
 ##################################################################################
-## multiple simulation fitting function
+## simulation fitting function with PCB data
 ##################################################################################
 # assume that b is aleardy standardized and normalized
 
@@ -168,6 +168,100 @@ compare_corr_GCTA <- function(b,
   colnames(result_raw) <- c("true_main", "true_interaction", "GCTA_main", "GCTA_interaction", "prop_main", "prop_interaction")
   result_raw
 }
+
+
+##################################################################################
+## simulation function with simulated covariate
+##################################################################################
+
+
+simulation_fn <- function(n,
+                          p,
+                          brep, 
+                          nrep,
+                          uncorr_method = NULL,
+                          interaction = 0, 
+                          interaction_m = 0, 
+                          seed = 0, 
+                          cores = 1,
+                          interm_result = FALSE,
+                          interm_result_path = NULL) {
+  if (cores == 1) 
+    foreach::registerDoSEQ() 
+  else 
+    doParallel::registerDoParallel(cores = cores) # setting cores
+
+  if(seed != 0) set.seed(seed) # set seed for foreach
+  
+  # Generate betas
+  betam=rnorm(p, mean =0, sd =0.5) # main_effect ~ N(0,0.5)
+  betam[2*c(1:floor(p/2))]=0  # mimic the zero coefficients
+  
+  if(interaction==0) {
+    betai <- 0
+  } else {
+    betai <- matrix(rnorm(p*p,m=0,sd=0.1),ncol=p) # interaction_effect ~ N(0,0.1)
+    betai[lower.tri(betai, diag = TRUE)] <- 0 # the number of interaction terms is {p*(p-1)}/2
+  } 
+
+  result_raw <- foreach(ibrep = 1:brep, .combine = rbind, .verbose = TRUE) %dorng%   {
+    result_tmp <- matrix(0, nrow = nrep, ncol = 6)
+    
+    # Generate covarite =s  
+    Sigma <- matrix(rep(0.6,34^2), ncol = 34)
+    diag(Sigma) <- 1
+    b <- mvrnorm(n = n,
+                 mu = rep(0,p),
+                 Sigma = Sigma)
+    
+    # Generate the signals
+    signalm=b%*%betam
+    signali <- if(interaction == 0){
+      rep(0,n) } else {
+        apply(X = b, MARGIN = 1, FUN = function(x) t(x)%*%betai%*%x)
+      } 
+    result_tmp[, 1]=var(signalm)
+    result_tmp[, 2]=var(signali)
+    
+    # Estimating total effects with iterations
+    for(irep in 1:nrep){
+      
+      # Generate health outcome given fixed random effects
+      y=signalm+signali+rnorm(n,sd=4)
+      
+      fit=Yang(y,b,interact = interaction_m)
+      result_tmp[irep,3] <- fit$G
+      result_tmp[irep,4] <- fit$RACT
+      
+      # uncorrelated data 
+      if(is.null(uncorr_method) == TRUE) 
+        x <- uncorr_fn(b)
+      else x <- uncorr_fn(b, uncorr_method)
+      
+      # Call the GCTA method
+      fit=Yang(y,x,interact = interaction_m)
+      result_tmp[irep,5] <- fit$G
+      result_tmp[irep,6] <- fit$RACT
+    }
+    
+    
+    if (interm_result == TRUE) {
+      common_attr_index <- match(c("dim", "dimnames"), names(attributes(b))) %>% na.omit(.)
+      interm_result_table <- data.frame(result_tmp, attributes(b)[-common_attr_index]) # extract the attributes which has unique infomration about the data
+      
+      write.csv(interm_result_table, file = paste0(interm_result_path, paste(unlist(attributes(b)[-common_attr_index]), collapse = "_"),"_",ibrep,".csv"))
+    }
+    
+    result_final <- rbind(apply(result_tmp, 2, mean), apply(result_tmp, 2,sd))
+    result_final
+  }
+  attributes(result_raw)$rng <- NULL # rm the random sampling info
+  # result_raw <- matrix(result_raw, nrow = 1)
+  colnames(result_raw) <- c("true_main", "true_interaction", "GCTA_main", "GCTA_interaction", "prop_main", "prop_interaction")
+  result_raw
+}
+
+
 
 ##################################################################################
 ## standardized function with tranformation features
---
+++ Chen_environmental_study/R_code/simulation_fixed_random/testing_code/testing_code.R
@@ -0,0 +1,31 @@
+# Following simulation is to test if under normal distribution, we can estimate the interaction effect unbaisly by proposed method
+
+setwd("~/dev/projects/Chen_environmental_study/")
+source("./R_code/Yang_REML.R")
+
+library(sas7bdat)
+library(MASS)
+library(tidyverse)
+library(foreach)
+library(doRNG)
+library(doParallel)
+
+
+test_result <-simulation_fn(n = 1000,
+                            p = 34,
+                            brep = 200,
+                            nrep = 10,
+                            interaction = 1, 
+                            interaction_m = 1,
+                            seed = 123,
+                            cores = 10)
+
+
+save(test_result, file = "./result/simulation_fixed_random_test")
+load(file = "./result/simulation_fixed_random_test")
+
+main <- test_result[test_result[,1] != 0, c(1,3,5)]
+tidyr::gather(main, key = "effect", value = "value") %>%
+  ggplot(., aes(x = effect, y = value, col = effect)) +
+  geom_histogram()
+inter <- test_result[test_result[,1] != 0, c(2,4,6)]
