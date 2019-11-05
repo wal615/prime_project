9624e50fe39d7d17fe44cbb8d405ccbfab87d82a
Xuelong Wang <xwang279@uic.edu>
2019-11-05 13:41:46 -0600

    -check the rrBlue and using interval checking method

---
+++ NSF_trafic/code/MetalonDA.R
@@ -0,0 +1,37 @@
+# # implement of ‘MetaLonDA’
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("metagenomeSeq")
+# BiocManager::install("DESeq2")
+# BiocManager::install("edgeR")
+# install.packages("MetaLonDA")
+library(devtools)
+install_github("aametwally/MetaLonDA", ref = "master")
+
+
+library(MetaLonDA)
+library(tidyverse)
+library(data.table)
+
+
+## Load read counts of 8 features from 100 samples. Samples are from 2 groups, 5 subjects per group, and 10 time points per subject.
+data(metalonda_test_data)
+View(metalonda_test_data[,1:20])
+
+## Create Group, Time, and ID annotation vectors
+n.group = 2
+n.sample = 5 
+n.timepoints = 10
+Group = factor(c(rep("A", n.sample*n.timepoints), rep("B",n.sample*n.timepoints)))
+Time = rep(rep(1:n.timepoints, times = n.sample), 2)
+ID = factor(rep(1:(2*n.sample), each = n.timepoints))
+
+## Define the prediction timeponits 
+points = seq(1, 10, length.out = 100)
+
+## fit the feature 5
+output.metalonda.f5 = metalonda(Count = metalonda_test_data[5,], Time = Time, Group = Group,
+                                ID = ID, n.perm = 100, fit.method = "nbinomial", points = points,
+                                text = rownames(metalonda_test_data)[5], parall = TRUE, pvalue.threshold = 0.05,
+                                adjust.method = "BH", time.unit = "days", ylabel = "Normalized Count",
+                                col = c("black", "green"), prefix = "Test_F5")
--- NSF_trafic/code/main.R
+++ NSF_trafic/code/main.R
@@ -23,7 +23,7 @@ t_s <- data.table(loaded_data$station.counts)
 colnames(t_s) <-loaded_data$station.ids 
 t_s[, time_stamp := loaded_data$station.times]
 t_s[, date := as.Date(time_stamp)]
-t_s[, hours := strftime(time_stamp, format="%H%M%S")]
+t_s[, hours := strftime(time_stamp, format="%H:%M:%S")]
 ## Incidents data 
 inc_s <- data.table(duration = loaded_data$inc.duration, time_stamp_start = loaded_data$inc.timestamp, 
                     ids =  loaded_data$inc.station.id)
@@ -47,38 +47,54 @@ t_s[,weekend := is.weekend(as.Date(time_stamp))]
 # ggplot(meltdf,aes(x=time_stamp,y=value,colour=variable,group=variable)) + geom_line()
 
 # calculate the mean and median for each holiday to see if there is any difference
-select_col <- names(t_s)[1:10]
-res_weekend_mean <- t_s[, lapply(.SD, FUN = median, na.rm = T),.SDcol = select_col, by = .(hours, weekend)] %>% copy(.) %>% 
-           gather(data = ., key = key, value = value, contains("S3"))
-ggplot(res_weekend_mean, aes(x = hours, y = value,
-                      group = interaction(weekend, key),
-                      colour = key, linetype = weekend)) +
-  geom_line() +
-  theme_bw()
+# select_col <- names(t_s)[1:10]
+# res_weekend_mean <- t_s[, lapply(.SD, FUN = median, na.rm = T),.SDcol = select_col, by = .(hours, weekend)] %>% copy(.) %>% 
+#            gather(data = ., key = key, value = value, contains("S3"))
+# ggplot(res_weekend_mean, aes(x = hours, y = value,
+#                       group = interaction(weekend, key),
+#                       colour = key, linetype = weekend)) +
+#   geom_line() +
+#   theme_bw()
+# 
+# res_weekend_var <- t_s[, lapply(.SD, FUN = function(x) {var(x, na.rm = T)/length(x)}),.SDcol = select_col, by = .(hours, weekend)] %>% copy(.) %>% 
+#   gather(data = ., key = key, value = value, contains("S3"))
+# ggplot(res_weekend_var, aes(x = hours, y = value,
+#                              group = interaction(weekend, key),
+#                              colour = key, linetype = weekend)) +
+#   geom_line() +
+#   theme_bw()
+# 
+# # calculate the mean and median for each holiday to see if there is any difference
+# select_col <- names(t_s)[1:10]
+# res_1 <- t_s[weekend == FALSE, lapply(.SD, FUN = mean, na.rm = T),.SDcol = select_col, by = .(hours, holiday)] %>% copy(.) %>% 
+#   gather(data = ., key = key, value = value, contains("S3"))
+# ggplot(res_1, aes(x = hours, y = value,
+#                   group = interaction(holiday, key),
+#                   colour = key, linetype = holiday)) +
+#   geom_line() +
+#   theme_bw()
+# 
+# res_holiday_var <- t_s[weekend == FALSE, lapply(.SD, FUN = function(x) {var(x, na.rm = T)/length(x)}),.SDcol = select_col, by = .(hours, holiday)] %>% copy(.) %>% 
+#   gather(data = ., key = key, value = value, contains("S3"))
+# ggplot(res_holiday_var, aes(x = hours, y = value,
+#                             group = interaction(holiday, key),
+#                             colour = key, linetype = holiday)) +
+#   geom_line() +
+#   theme_bw()
 
-res_weekend_var <- t_s[, lapply(.SD, FUN = function(x) {var(x, na.rm = T)/length(x)}),.SDcol = select_col, by = .(hours, weekend)] %>% copy(.) %>% 
-  gather(data = ., key = key, value = value, contains("S3"))
-ggplot(res_weekend_var, aes(x = hours, y = value,
-                             group = interaction(weekend, key),
-                             colour = key, linetype = weekend)) +
-  geom_line() +
-  theme_bw()
+## MetaLonDA
+library(MetaLonDA)
 
-# calculate the mean and median for each holiday to see if there is any difference
-select_col <- names(t_s)[1:10]
-res_1 <- t_s[weekend == FALSE, lapply(.SD, FUN = mean, na.rm = T),.SDcol = select_col, by = .(hours, holiday)] %>% copy(.) %>% 
-  gather(data = ., key = key, value = value, contains("S3"))
-ggplot(res_1, aes(x = hours, y = value,
-                  group = interaction(holiday, key),
-                  colour = key, linetype = holiday)) +
-  geom_line() +
-  theme_bw()
+# modify the data set
+test <- copy(t_s)
+test[, group := ifelse(weekend, "weekend", "weekday")]
+test[,time_seq_min := difftime(as.POSIXct(hours, format="%H:%M:%S"), as.POSIXct("00:00:00", format="%H:%M:%S"), units = "mins") %>% as.numeric(.)]
+test[,id := as.numeric(date)]
+one_month_data <- copy(test[id <= 17197,])
 
-res_holiday_var <- t_s[weekend == FALSE, lapply(.SD, FUN = function(x) {var(x, na.rm = T)/length(x)}),.SDcol = select_col, by = .(hours, holiday)] %>% copy(.) %>% 
-  gather(data = ., key = key, value = value, contains("S3"))
-ggplot(res_holiday_var, aes(x = hours, y = value,
-                            group = interaction(holiday, key),
-                            colour = key, linetype = holiday)) +
-  geom_line() +
-  theme_bw()
-
+# fit the model 
+output.S312425 = metalonda(Count = one_month_data$S312425, Time = one_month_data$time_seq_min, Group = one_month_data$group,
+                                ID = one_month_data$id, n.perm = 40, fit.method = "nbinomial", points = one_month_data$time_seq_min,
+                                text = "S312425", parall = TRUE, pvalue.threshold = 0.05,
+                                adjust.method = "BH", time.unit = "mins", ylabel = "Normalized Count",
+                                col = c("black", "green"), prefix = "S312425_one_month")
